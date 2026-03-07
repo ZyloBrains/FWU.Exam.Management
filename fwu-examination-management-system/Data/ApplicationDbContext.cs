f1af71df1e6bab1f3d9a89ef3c480859c92fec55
Harikrishna Gautam <93207808+Harikrishna-HG@users.noreply.github.com>
2026-03-07 17:06:09 +0545

    Add project files.

---
+++ fwu-examination-management-system/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace fwu_examination_management_system.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ fwu-examination-management-system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fwu_examination_management_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
