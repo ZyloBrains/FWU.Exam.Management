cc3c7ff03fe11a470f785e01b0d0bf6cf87e5bec
Bishnu Rawal <rawal.bishnu@gmail.com>
2026-03-09 15:15:04 +0545

    Add organization and appuser

---
+++ fwu-examination-management-system/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace fwu_examination_management_system.Models;
+public class AppUser: IdentityUser
+{
+    public int OrganizationId { get; set; }
+    public Organization? Organization { get; set; }
+}
---
+++ fwu-examination-management-system/Models/Organization.cs
@@ -0,0 +1,11 @@
+namespace fwu_examination_management_system.Models;
+public class Organization
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string OfficeCode { get; set; }
+    public string ContactNumber { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string LogoPath { get; set; }
+}
