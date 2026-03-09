943c329580ab137f53840cbbaf3f781694bf6bea
Harikrishna Gautam <93207808+Harikrishna-HG@users.noreply.github.com>
2026-03-09 18:11:02 +0545

    Add ProfilePath to users and make LogoPath nullable
    Added migration to introduce a nullable ProfilePath column to AspNetUsers for storing user profile image paths. Also updated Organizations table to allow LogoPath to be nullable. Updated model snapshot and migration designer accordingly.

---
+++ fwu-examination-management-system/Data/Migrations/20260309122502_AddProfilePathToUser.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fwu_examination_management_system.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePathToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LogoPath",
+                table: "Organizations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePath",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePath",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LogoPath",
+                table: "Organizations",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
