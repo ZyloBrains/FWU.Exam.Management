6f726257afd8b3d05ada5fd09294ce8ee8f153a6
Harikrishna Gautam <harishgautam2004@gmail.com>
2026-07-15 22:59:24 +0545

    Add migration: Move ControllerSignaturePath from Faculty to Tenant

---
+++ FWU.Exam.Management.Infrastructure/Migrations/20260715171056_MoveControllerSignatureToTenant.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FWU.Exam.Management.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveControllerSignatureToTenant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ControllerSignaturePath",
+                table: "Faculties");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ControllerSignaturePath",
+                table: "Tenants",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ControllerSignaturePath",
+                table: "Tenants");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ControllerSignaturePath",
+                table: "Faculties",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
