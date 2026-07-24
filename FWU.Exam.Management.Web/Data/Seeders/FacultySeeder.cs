ce14fdb5761100a9f15065b40367701b32217570
Harikrishna Gautam <harishgautam2004@gmail.com>
2026-07-24 15:12:11 +0545

    Remove Id property from Faculty seeder entries for consistency

--- FWU.Exam.Management.Web/Data/Seeders/FacultySeeder.cs
+++ FWU.Exam.Management.Web/Data/Seeders/FacultySeeder.cs
@@ -15,15 +15,15 @@ public static class FacultySeeder
 
         var faculties = new[]
         {
-            new Faculty { Id = 1, Name = "Agriculture Science", OfficeCode = "L103", ShortName = "AG" },
-            new Faculty { Id = 2, Name = "Education", OfficeCode = "L006", ShortName = "ED" },
-            new Faculty { Id = 3, Name = "Engineering", OfficeCode = "L091", ShortName = "EG" },
-            new Faculty { Id = 4, Name = "Humanities and Social Sciences", OfficeCode = "L003", ShortName = "HU" },
-            new Faculty { Id = 5, Name = "Law", OfficeCode = "L140", ShortName = "LW" },
-            new Faculty { Id = 6, Name = "Management", OfficeCode = "L001", ShortName = "MG" },
-            new Faculty { Id = 7, Name = "Science and Technology", OfficeCode = "L002", ShortName = "SC" },
-            new Faculty { Id = 8, Name = "Health Sciences", OfficeCode = "L010", ShortName = "HS" },
-            new Faculty { Id = 9, Name = "Natural Resource Management", OfficeCode = "L011", ShortName = "NR" },
+            new Faculty { Name = "Agriculture Science", OfficeCode = "L103", ShortName = "AG" },
+            new Faculty { Name = "Education", OfficeCode = "L006", ShortName = "ED" },
+            new Faculty { Name = "Engineering", OfficeCode = "L091", ShortName = "EG" },
+            new Faculty { Name = "Humanities and Social Sciences", OfficeCode = "L003", ShortName = "HU" },
+            new Faculty { Name = "Law", OfficeCode = "L140", ShortName = "LW" },
+            new Faculty { Name = "Management", OfficeCode = "L001", ShortName = "MG" },
+            new Faculty { Name = "Science and Technology", OfficeCode = "L002", ShortName = "SC" },
+            new Faculty { Name = "Health Sciences", OfficeCode = "L010", ShortName = "HS" },
+            new Faculty { Name = "Natural Resource Management", OfficeCode = "L011", ShortName = "NR" },
         };
 
         await context.Faculties.AddRangeAsync(faculties);
