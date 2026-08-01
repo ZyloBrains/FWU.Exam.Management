a257bdf223929969c201e2e03fea23315310a6db
Bishnu Rawal <rawal.bishnu@gmail.com>
2026-08-01 12:37:11 +0545

    remove unused seedeers

--- FWU.Exam.Management.Web/Data/Seeders/AcademicStructureSeeder.cs
+++
@@ -1,8 +0,0 @@
-namespace FWU.Exam.Management.Web.Data.Seeders;
-
-public static class AcademicStructureSeeder
-{
-    public static async Task SeedAcademicStructureAsync(IServiceProvider serviceProvider)
-    {
-    }
-}
