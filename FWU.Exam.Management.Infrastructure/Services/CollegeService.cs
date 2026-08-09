07e30f07a72ae081253cdb02784f9d93ec9bd049
Bishnu Rawal <rawal.bishnu@gmail.com>
2026-08-09 18:30:43 +0545

    Render faculties on colleges UI

--- FWU.Exam.Management.Infrastructure/Services/CollegeService.cs
+++ FWU.Exam.Management.Infrastructure/Services/CollegeService.cs
@@ -50,6 +50,7 @@ public class CollegeService(AppDbContext context, IUserContext userContext) : IC
         return await context.Colleges
             .Include(c => c.CollegeType)
             .Include(c => c.CollegeFaculties)
+            .ThenInclude(cf => cf.Faculty)
             .Include(c => c.Address)
             .ThenInclude(a => a!.LocalLevel)
             .ThenInclude(ll => ll!.District)
@@ -224,6 +225,8 @@ public class CollegeService(AppDbContext context, IUserContext userContext) : IC
     {
         var query = context.Colleges
             .Include(c => c.CollegeType)
+            .Include(c => c.CollegeFaculties)
+            .ThenInclude(cf => cf.Faculty)
             .Include(c => c.Address)
             .ThenInclude(a => a!.LocalLevel)
             .ThenInclude(ll => ll!.District)
@@ -255,6 +258,7 @@ public class CollegeService(AppDbContext context, IUserContext userContext) : IC
             "name" => c => c.Name,
             "shortname" => c => c.ShortName ?? "",
             "district" => c => (c.Address != null && c.Address.LocalLevel != null && c.Address.LocalLevel.District != null) ? c.Address.LocalLevel.District.DistrictName : "",
+            "faculties" => c => c.CollegeFaculties.OrderBy(cf => cf.Faculty == null ? "" : cf.Faculty.Name).Select(cf => cf.Faculty == null ? "" : cf.Faculty.Name).FirstOrDefault() ?? "",
             "collegetype" => c => c.CollegeType != null ? c.CollegeType.Code : "",
             "displayorder" => c => c.DisplayOrder ?? 0,
             "isactive" => c => c.IsActive,
